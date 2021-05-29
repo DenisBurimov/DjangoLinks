c4834bc7bac280794b1cf0e4f94208222fe57f2a
Denys <denysburimov@gmail.com>
2021-05-29 07:38:17 +0200

    links_app testing

---
+++ linksservice/links/links.py
@@ -0,0 +1,8 @@
+import pyshorteners
+
+# https://www.youtube.com/watch?v=6DhSLGPsyq4&t=197s
+long_link = input("Your link: ")
+cut = pyshorteners.Shortener()
+short_link = cut.tinyurl.short(long_link)
+
+print(short_link)
